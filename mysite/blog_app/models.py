90790ad0c09569a7eb1b43afd15bfe75802b9f03
faridhavasi <faridhavasi@yahoo.com>
2022-07-22 01:28:16 +0430

    add model Articel

--- mysite/blog_app/models.py
+++ mysite/blog_app/models.py
@@ -1,3 +1,20 @@
+
 from django.db import models
 
 # Create your models here.
+class Artcel(models.Model):
+    STATUS_CHIOCES=(
+        ('p','publish'),
+        ('d','dradt'),
+    )
+    title=models.CharField(max_length=20)
+    description=models.TextField(max_length=50)
+    image=models.ImageField(null=True,upload_to='blog_app')
+    activate=models.BooleanField(default=True)
+    status=models.CharField(default='d',choices=STATUS_CHIOCES)
+    views=models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return self.title
+
