038c45d9aac6420f1390084918d5db5442466fb7
faridhavasi <faridhavasi@yahoo.com>
2022-07-22 17:16:52 +0430

    add articel_list view

--- mysite/blog_app/views.py
+++ mysite/blog_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Artcel
+
+def Articel_list(request):
+    articel=Artcel.objects.all() # qery or orm
+    return render (request,'blog_app/Artcel_list.html',context={'articel':articel})
+     
