1f50ffb0259dcb488e1c9e57327888eb857a84fe
faridhavasi <faridhavasi@yahoo.com>
2022-07-24 20:55:25 +0430

    add model Ticket

--- mysite/ticket_app/models.py
+++ mysite/ticket_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Ticket(models.Model):
+    title=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+    description=models.TextField()
+
+
+    def __str__(self):
+        return self.title
+    
