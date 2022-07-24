1599ca507d59d0cbd94e886e79e289128215d608
faridhavasi <faridhavasi@yahoo.com>
2022-07-24 21:01:28 +0430

    config url ticket_app

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog_app.urls')),
+    path('ticket',include('ticket_app.urls'))
 ]  +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
