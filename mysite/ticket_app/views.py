83dfd8041fb162e4f1d924302d5bd5f21b2ce872
faridhavasi <faridhavasi@yahoo.com>
2022-07-24 21:38:45 +0430

    add ticeket view

--- mysite/ticket_app/views.py
+++ mysite/ticket_app/views.py
@@ -1,3 +1,13 @@
+
 from django.shortcuts import render
 
-# Create your views here.
+from . models import Ticket
+
+def add_ticket(request):
+    title=request.GET.get('title')
+    email=request.GET.get('email')
+    description=request.GET.get('description')
+    if title and email and description:
+        Ticket.objects.create(title=title,email=email,description=description)
+
+    return render (request,'ticket_app/add_ticket.html')    
