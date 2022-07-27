55c63ce47796b8bc124009896d6dba052a04f689
faridhavasi <faridhavasi@yahoo.com>
2022-07-27 16:03:08 +0430

    add method post in tivket

--- mysite/ticket_app/views.py
+++ mysite/ticket_app/views.py
@@ -4,10 +4,11 @@ from django.shortcuts import render
 from . models import Ticket
 
 def add_ticket(request):
-    title=request.GET.get('title')
-    email=request.GET.get('email')
-    description=request.GET.get('description')
-    if title and email and description:
-        Ticket.objects.create(title=title,email=email,description=description)
+    if request.method=="POST":
+        title=request.POST.get('title')
+        email=request.POST.get('email')
+        description=request.POST.get('description')
+        if title and email and description:
+            Ticket.objects.create(title=title,email=email,description=description)
 
     return render (request,'ticket_app/add_ticket.html')    
